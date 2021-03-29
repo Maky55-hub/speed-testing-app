5e856a0ec6de932347bdb2e73a6cab7b4f03219d
Maky55-hub <57489140+Maky55-hub@users.noreply.github.com>
2021-03-29 09:09:52 -0700

    custom css added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactListComponent } from './components/pages/contact-list/contact-list.component';
+import { ContactDetailComponent } from './components/pages/contact-detail/contact-detail.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'contacts',
+    component: ContactListComponent
+  },
+  {
+    path: 'contact/:id',
+    component: ContactDetailComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatSliderModule } from '@angular/material/slider';
+import { 
+  MatSliderModule,
+  MatToolbarModule,
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatCheckboxModule,
+  MatDatepickerModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatListModule,
+  MatRadioModule,
+  MatIconModule
+} from '@angular/material';
 import { ContactListComponent } from './components/pages/contact-list/contact-list.component';
 import { ContactDetailComponent } from './components/pages/contact-detail/contact-detail.component';
 import { NameInputComponentComponent } from './components/partials/name-input-component/name-input-component.component';
@@ -25,7 +37,17 @@ import { EmployeeListComponentComponent } from './components/partials/employee-l
     BrowserAnimationsModule,
 
     // Material Imports
-    MatSliderModule
+    MatSliderModule,
+    MatToolbarModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatListModule,
+    MatRadioModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
